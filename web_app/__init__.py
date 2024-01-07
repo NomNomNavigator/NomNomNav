f51094e9142b7ece36663534bb52644c5404c90f
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2024-01-06 20:47:52 -0500

    updated code for now

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -24,4 +24,12 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(confirm)
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    # @login_manager.user_loader
+    # def load_user(_id):
+    #     return User.query.get(int(_id))
+
     return app
--- web_app/views.py
+++ web_app/views.py
@@ -3,6 +3,13 @@ from flask import Blueprint, render_template
 views = Blueprint('views', __name__)
 
 
+# route for users that are not yet logged in or who have not signed up
 @views.route('/')
+def access():
+    return render_template('base.html')
+
+
+# home page of the users that are logged in
+@views.route('/home')
 def home():
     return render_template('home.html')
