a64e1322268e70b17149d0423337f13f7c8f5c95
Corinne <CorinneKogut@gmail.com>
2024-01-07 19:55:27 -0500

    changed password method

--- web_app/models.py
+++ web_app/models.py
@@ -15,7 +15,7 @@ from . import db
 # For user sign-up, login, session tracking, preferences, feedback tracking
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(25), unique=True, nullable=False)
+    user_name = db.Column(db.String(25), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
