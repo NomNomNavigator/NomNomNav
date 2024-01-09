866d8767bd69b9314d23edb9ad4cefa60028b37b
Mike <mike@Zipcoders-MacBook-Pro-83.local>
2024-01-09 14:48:33 -0500

    sqlalchemy 2.0 query style implemented

--- web_app/ranker.py
+++ web_app/ranker.py
@@ -4,7 +4,7 @@ MVP:  Hard code some basic rules
 """
 from . import models, db
 from models import Restaurant, User
-
+from sqlalchemy import select
 
 
 # Function to get all the data about a user needed to recommend restaurants
@@ -13,11 +13,13 @@ def get_restaurant_recos(db, user):
     pass
 
 
-def get_agg_rating(pos_restaurants: str):
+# Function to figure out from user liked restaurants what their preferred price range might be.
+# We could instead ask them to enter a preferred price range in their profile -> need to add column to model/migrate
+def get_agg_rating(pos_restaurants: str) -> float:
     p_rest_list = pos_restaurants.split(',')
     r_sum, r_count = 0, 0
     for r in p_rest_list:
-        r_rating = db.query(Restaurant).filterby(Restaurant.id == int(r)).first()
+        r_rating = db.session.execute(select(Restaurant.avg_rating).where(Restaurant.id == int(r)))
         r_sum = r_sum + r_rating
         r_count += 1
     agg_rating = r_sum / r_count
@@ -27,33 +29,20 @@ def get_agg_rating(pos_restaurants: str):
 
 # Figure out if should be using a session with the DB for this function or pieces within,
 # and if that needs Session = Depends()
-def gather_ranking_data(user_id: int, sel_cuisine: str, ambiance: str, price_range: str, pos_restaurants: str):
+def gather_ranking_data(user_id: int, cuisine: str, ambiance: str, price_range: str, pos_restaurants: str):
 
-    if not sel_cuisine:
-        pref_cuisine = db.query(User.pref_cuisine).filterby(User.id == user_id).first()
+    if not cuisine:
+        cuisine = db.session.execute(select(User.pref_cuisine).where(User.id == user_id))
 
     if not price_range:
-        pref_price_range = db.query(User.pref_price_range).filterby(User.id == user_id).first()
+        price_range = db.session.execute(select(User.pref_price_range).where(User.id == user_id))
 
     if not ambiance:
-        pref_ambiance = db.query(User.pref_ambiance).filterby(User.id == user_id).first()
+        ambiance = db.session.execute(select(User.pref_ambiance).where(User.id == user_id))
 
     avg_rating = get_agg_rating(pos_restaurants)
 
-
-
-
-
-    restaurants = db.query(Restaurant)
-
-    for restaurant in restaurants:
-
-        avg_rating
-        review_count
-        price_range
-
-
-
-
-
-
+    # Search:  Restaurants with a matching cuisine, exact matching price range, avg_rating within .5
+    search_match_restaurants = db.session.execute(
+    # Sort/Rank: If ambiance exists, rank match first.
+    # Sort/Rank: Avg_rating high to low.
--- web_app/selected.py
+++ web_app/selected.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
 
-views = Blueprint('views', __name__)
+views = Blueprint('selected_restaurant', __name__)
 
 
 # route for users that are not yet logged in or who have not signed up
 @login_required
 @views.route('/recommendation/{restaurant}', methods=['POST'])
-def get_restaurant_details():
+def get_restaurant_details(restaurant_id, user_id):
     return render_template('selected_restaurant.html')
 
 
