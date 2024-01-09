eed4e00706fedbc38d3dec5213e508dd5cc0fb77
Mike <mike@Zipcoders-MacBook-Pro-83.local>
2024-01-09 11:47:32 -0500

    show recos template, selected_restaurant route/templates

--- web_app/return_recos.py
+++ web_app/return_recos.py
@@ -6,9 +6,9 @@ views = Blueprint('views', __name__)
 
 # route for users that are not yet logged in or who have not signed up
 @login_required
-@views.route('/recommendations', methods=['POST'])
-def get_recos():
-    return render_template('index.html')
+@views.route('/recommendation/{restaurant}', methods=['POST'])
+def get_restaurant_details():
+    return render_template('selected_restaurant.html')
 
 
 
